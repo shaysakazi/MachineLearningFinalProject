4d23980af1e1aefb0062fdd97f0a86cf81420755
Shay Sakazi <sakazis@post.bgu.ac.il>
2020-09-12 16:54:52 +0300

    finish egp

--- src/ensemble_genetic_programming.py
+++ src/ensemble_genetic_programming.py
@@ -1,34 +1,54 @@
 import random
-import re
-import pandas as pd
+from collections import Counter
+from datetime import datetime
+
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor, VotingRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.exceptions import NotFittedError
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+import copy
 
 
 class EnsembleGeneticProgramming:
-    def __init__(self, num_trees=100, num_forest=10, weighted=False, max_generations=10, forest_selection=5, k=5):
+    def __init__(self, num_trees=100, num_forest=100, max_generations=10, k=3):
         self.num_trees = num_trees
         self.num_forest = num_forest
-        self.weighted = weighted
         self.max_generations = max_generations
-        self.forest_selection = forest_selection
         self.k = k
         self.voting_forests = None
 
     def _egp(self, X, y):
         forest_list = self._generate_forests(X)  # list of (str, estimator) tuples
         g = 0
-        while g < self.max_generations or len(forest_list) == 1:
+        while g < self.max_generations:
+            # print(f"Generation index: {g}, number of forests: {len(forest_list)},"
+            #       f" max_generations: {self.max_generations}")
+            time = datetime.now()
             forest_parents = self._select_forests(forest_list, X, y)  # choose N forests
-            forest_off_springs = self._breeding(forest_parents)  # create N off springs
-            forest_list = self._prune(forest_off_springs, X, y)  # prune 2N (N from forest selection and N from off
+            time = datetime.now() - time
+            # print(f"Finish selection on Generation index: {g} after {time.seconds} seconds")
+            time = datetime.now()
+            forest_offsprings = self._breeding(forest_parents)  # create N off springs
+            time = datetime.now() - time
+            # print(f"Finish breeding on Generation index: {g} after {time.seconds} seconds")
+            time = datetime.now()
+            forest_list = self._prune(forest_offsprings, X, y)  # prune 2N (N from forest selection and N from off
             # springs) into N forests
+            time = datetime.now() - time
+            # print(f"Finish pruning on Generation index: {g} after {time.seconds} seconds")
             g += 1
 
-        return VotingRegressor(forest_list)
+        return forest_list
+
+    def _best_model(self, forest_list, X, y):
+        forests_scores = {forest_offspring: self._eval_forest(forest_offspring, X, y)
+                                   for forest_offspring in forest_list}
+
+        count_dict = Counter(forests_scores)
+        best_forest = count_dict.most_common(1)
+
+        dataset_features = best_forest[0][7:best_forest[0].find('END')-2].split(', ')
+        return best_forest[0], best_forest[1].fit(X[dataset_features], y)
 
     def _generate_forests(self, X):
         forest_list = []
@@ -63,7 +83,7 @@ class EnsembleGeneticProgramming:
                  f'{max_depth}, {min_samples_split}, {min_weight_fraction_leaf}, {max_features}, {random_state}'
         rf_reg = RandomForestRegressor(n_estimators=self.num_trees, max_depth=max_depth, random_state=random_state,
                                        min_samples_split=min_samples_split, max_features=max_features,
-                                       min_weight_fraction_leaf=min_weight_fraction_leaf, n_jobs=-1)
+                                       min_weight_fraction_leaf=min_weight_fraction_leaf)
 
         return rf_str, rf_reg
 
@@ -80,8 +100,11 @@ class EnsembleGeneticProgramming:
 
     def _select_forests(self, forest_list, X, y):
         forest_parents = []
-        for i in range(self.num_forest):
+        for _ in range(self.num_forest):
+            # time = datetime.now()
             selected_forest = self._tournament_selection(forest_list, X, y)
+            # time = datetime.now() - time
+            # print(f"_tournament_selection after {time.seconds} seconds")
             forest_parents.append(selected_forest)
 
         return forest_parents
@@ -91,30 +114,31 @@ class EnsembleGeneticProgramming:
         best_mse = 0
         for i in range(self.k):
             ind = random.choice(forest_list)
+            # time = datetime.now()
             ind_mse = self._eval_forest(ind, X, y)
+            # time = datetime.now() - time
+            # print(f"_eval_forest after {time.seconds} seconds")
             if best is None or ind_mse > best_mse:
                 best = ind
                 best_mse = ind_mse
         return best
 
     def _breeding(self, forest_parents):
+        offsprings = copy.deepcopy(forest_parents)
         for i in range(self.num_forest):
-            off_spring = self._breed(forest_parents)
-            forest_parents.append(off_spring)
-        return forest_parents
+            offspring = self._breed(forest_parents)
+            offsprings.append(offspring)
+        return offsprings
 
     def _breed(self, forest_parents):
         parent_1 = random.choice(forest_parents)
         parent_2 = random.choice(forest_parents)
 
-        off_spring = self._uniform_crossover(parent_1, parent_2)
-        print(off_spring[1])
-        # mutation_prob = random.random()
-        mutation_prob = 0
+        offspring = self._uniform_crossover(parent_1, parent_2)
+        mutation_prob = random.random()
         if mutation_prob < 0.5:
-            off_spring = self._mutate(off_spring)
-        print(off_spring[1])
-        return off_spring
+            offspring = self._mutate(offspring)
+        return offspring
 
     def _uniform_crossover(self, parent_1, parent_2):
         offspring_forest_features = {'max_depth': None, 'min_samples_split': None, 'min_weight_fraction_leaf': None,
@@ -122,7 +146,7 @@ class EnsembleGeneticProgramming:
         parent_1_forest_features = {'max_depth': parent_1[1].max_depth,
                                     'min_samples_split': parent_1[1].min_samples_split,
                                     'min_weight_fraction_leaf': parent_1[1].min_weight_fraction_leaf,
-                                     'max_features': parent_1[1].max_features, 'random_state': parent_1[1].random_state}
+                                    'max_features': parent_1[1].max_features, 'random_state': parent_1[1].random_state}
         parent_2_forest_features = {'max_depth': parent_2[1].max_depth,
                                     'min_samples_split': parent_2[1].min_samples_split,
                                     'min_weight_fraction_leaf': parent_2[1].min_weight_fraction_leaf,
@@ -135,8 +159,8 @@ class EnsembleGeneticProgramming:
             else:
                 offspring_forest_features[key] = parent_2_forest_features[key]
 
-        selected_parent_1_dataset_features = re.search('START, (.*), END', parent_1[0]).group(1).split(', ')
-        selected_parent_2_dataset_features = re.search('START, (.*), END', parent_2[0]).group(1).split(', ')
+        selected_parent_1_dataset_features = parent_1[0][7:parent_1[0].find('END')-2].split(', ')
+        selected_parent_2_dataset_features = parent_2[0][7:parent_2[0].find('END')-2].split(', ')
 
         offspring_dataset_features = list(set(selected_parent_1_dataset_features + selected_parent_2_dataset_features))
 
@@ -149,12 +173,11 @@ class EnsembleGeneticProgramming:
                                        random_state=offspring_forest_features['random_state'],
                                        min_samples_split=offspring_forest_features['min_samples_split'],
                                        max_features=offspring_forest_features['max_features'],
-                                       min_weight_fraction_leaf=offspring_forest_features['min_weight_fraction_leaf'],
-                                       n_jobs=-1)
+                                       min_weight_fraction_leaf=offspring_forest_features['min_weight_fraction_leaf'])
 
         return rf_str, rf_reg
 
-    def _mutate(self, off_spring):
+    def _mutate(self, offspring):
         mutate_feature = random.choice(['max_depth', 'min_samples_split', 'min_weight_fraction_leaf',
                                         'max_features', 'random_state'])
         forest_features = {'max_depth': range(2, 10),
@@ -162,44 +185,91 @@ class EnsembleGeneticProgramming:
                            'min_weight_fraction_leaf': np.linspace(0.05, 0.5, 10),
                            'max_features': ['auto', 'sqrt', 'log2'],
                            'random_state': range(2, 100)}
-        print(mutate_feature)
-        off_spring[1].__setattr__(mutate_feature, random.choice(forest_features[mutate_feature]))
-        return off_spring
 
-    def _prune(self, forest_off_springs, X, y):
-        # TODO - STOP HERE
-        forest_list = []
-        # choose randomly 5% of the forests
-        # the rest take the best forests
+        offspring_forest_features = dict(max_depth=offspring[1].max_depth,
+                                         min_samples_split=offspring[1].min_samples_split,
+                                         min_weight_fraction_leaf=offspring[1].min_weight_fraction_leaf,
+                                         max_features=offspring[1].max_features, random_state=offspring[1].random_state)
 
-        return self.num_trees
+        offspring_forest_features[mutate_feature] = random.choice(forest_features[mutate_feature])
+        offspring_str = offspring[0][:offspring[0].find('END') + 5] + \
+                       f"{offspring_forest_features['max_depth']}, {offspring_forest_features['min_samples_split']}," \
+                       f" {offspring_forest_features['min_weight_fraction_leaf']}," \
+                       f" {offspring_forest_features['max_features']}, {offspring_forest_features['random_state']}"
+
+        offspring[1].__setattr__(mutate_feature, offspring_forest_features[mutate_feature])
+
+        return offspring_str, offspring[1]
+
+    def _prune(self, forest_offsprings, X, y):
+        forest_offsprings_score = {forest_offspring: self._eval_forest(forest_offspring, X, y)
+                                   for forest_offspring in forest_offsprings}
+
+        count_dict = Counter(forest_offsprings_score)
+        prune_forest = count_dict.most_common(self.num_forest)
+        # print(f"minimum mse forest is: {min(count_dict, key=count_dict.get)[0]}")
+        # print(f"minimum mse forest: {count_dict[min(count_dict, key=count_dict.get)]:.4f}")
+        # print(f"maximum mse forest is: {max(count_dict, key=count_dict.get)[0]}")
+        # print(f"maximum mse forest: {count_dict[max(count_dict, key=count_dict.get)]:.4f}\n")
+
+        return [item[0] for item in prune_forest]
 
     def _eval_forest(self, ind, X, y):
-        dataset_features = re.search('START, (.*), END', ind[0]).group(1).split(', ')
+        dataset_features = ind[0][7:ind[0].find('END')-2].split(', ')
         forest = ind[1]
+        mse_fold = cross_val_score(forest, X[dataset_features], y, scoring='neg_mean_squared_error', cv=2)
+        # kf = KFold(n_splits=3, shuffle=True)
+        # mse_fold = []
+        # for train_index, val_index in kf.split(X[dataset_features]):
+        #     X_train, X_val = X[dataset_features].iloc[train_index], X[dataset_features].iloc[val_index]
+        #     y_train, y_val = y.iloc[train_index], y.iloc[val_index]
+        #
+        #     forest.fit(X_train, y_train)
+        #     y_pred = forest.predict(X_val)
+        #
+        #     mse_fold.append(mean_squared_error(y_val, y_pred))
 
-        X_train, X_test, y_train, y_test = train_test_split(X[dataset_features], y, test_size=0.2, shuffle=True)
-        forest.fit(X_train, y_train)
-        y_pred = forest.predict(X_test)
+        # print(f"forest str: {ind[0]}")
+        # print(f"forest mse: {np.mean(mse_fold):.4f} +/- {np.std(mse_fold):.4f}")
 
-        return mean_squared_error(y_test, y_pred)
+        return np.mean(mse_fold) + np.std(mse_fold)
 
     def fit(self, X, y):
+        # print("===START===")
         self.voting_forests = self._egp(X, y)
+        for forest in self.voting_forests:
+            dataset_features = forest[0][7:forest[0].find('END')-2].split(', ')
+            forest[1].fit(X[dataset_features], y)
+        # print("===END===")
 
     def predict(self, X):
         if self.voting_forests is None:
             raise NotFittedError("This EnsembleGeneticProgramming instance is not fitted yet. "
                                  "Call 'fit' with appropriate arguments before using this estimator.")
-        else:
-            pass
+
+        predict_values = []
+        for forest in self.voting_forests:
+            dataset_features = forest[0][7:forest[0].find('END')-2].split(', ')
+            predict_values.append(forest[1].predict(X[dataset_features]))
+
+        columns = zip(*predict_values)
+        return np.array([np.mean([x for x in c]) for c in columns])
 
     def score(self, X, y):
-        pass
+        if self.voting_forests is None:
+            raise NotFittedError("This EnsembleGeneticProgramming instance is not fitted yet. "
+                                 "Call 'fit' with appropriate arguments before using this estimator.")
+        predict_scores = []
+        for forest in self.voting_forests:
+            dataset_features = forest[0][7:forest[0].find('END')-2].split(', ')
+            predict_scores.append(forest[1].score(X[dataset_features], y))
+        return np.mean(predict_scores)
 
+    def get_params(self, deep=True):
+        return {"num_trees": self.num_trees, "num_forest": self.num_forest, "max_generations": self.max_generations,
+                "k": self.k}
 
-
-
-
-
-
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
--- src/main.py
+++ src/main.py
@@ -1,27 +1,121 @@
-from sklearn.ensemble import RandomForestRegressor
-from src.ensemble_genetic_programming import EnsembleGeneticProgramming
+import os
+import sys
+from datetime import datetime
+from sklearn.metrics import mean_squared_error, mean_absolute_error, median_absolute_error, r2_score, \
+    explained_variance_score
+from sklearn.model_selection import train_test_split, KFold, RandomizedSearchCV
+from sklearn.tree import ExtraTreeRegressor
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+sys.path.insert(1, '/home/sakazis/workspace/MachineLearningFinalProject/')
+from src.ensemble_genetic_programming import EnsembleGeneticProgramming
 
 home_dir = "/Users/shaysakazi/PycharmProjects/MachineLearningFinalProject"
-server_dir = None
+server_dir = '/home/sakazis/workspace/MachineLearningFinalProject/'
 working_dir = home_dir
+
+# def test():
+#     df = pd.read_csv(f'{working_dir}/regressionDatasets/Admission_Predict_kaggle.csv')
+#     X = df.iloc[:, :-1]
+#     y = df.iloc[:, -1]
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+#
+#     egp = EnsembleGeneticProgramming()
+#     egp.fit(X_train, y_train)
+#     egp_y_pred = egp.predict(X_test)
+#     print(f"egp prediction mse: {mean_squared_error(y_test, egp_y_pred):.4f}")
+#     print(f"egp prediction score: {egp.score(X_test, y_test):.4f}")
+#
+#     etr = ExtraTreeRegressor()
+#     etr.fit(X_train, y_train)
+#     etr_y_pred = etr.predict(X_test)
+#     print(f"etr prediction mse: {mean_squared_error(y_test, etr_y_pred):.4f}")
+#     print(f"etr prediction score: {etr.score(X_test, y_test):.4f}")
+#
+#     rfr = RandomForestRegressor()
+#     rfr.fit(X_train, y_train)
+#     rfr_y_pred = rfr.predict(X_test)
+#     print(f"rfr prediction mse: {mean_squared_error(y_test, rfr_y_pred):.4f}")
+#     print(f"rfr prediction score: {rfr.score(X_test, y_test):.4f}")
+
+
+def eval_dataset(dataset_path, model_name, egp_hyper_parameters, egp_str):
+    dataset = pd.read_csv(f'{working_dir}/regressionDatasets/{dataset_path}')
+    test_data = []
+    X = dataset.iloc[:, :-1]
+    X = pd.get_dummies(X)
+    y = dataset.iloc[:, -1]
+
+    outer_kf = KFold(n_splits=10, shuffle=True)
+    outer_fold_index = 1
+
+    for train_index, test_index in outer_kf.split(X):
+        print(f'{dataset_path}, model: {model_name}, fold_number: {outer_fold_index}')
+        test_cross_data = {'Dataset Name': dataset_path[:dataset_path.find('.')], 'Algorithm Name': model_name,
+                           'Cross Validation': outer_fold_index, 'Hyper-Parameters Values': None,
+                           'Mean Squared Error': None, 'Mean Absolute Error': None, 'Median Absolute Error': None,
+                           'R2 Score': None, 'Explained Variance Score': None, 'Training Time': None,
+                           'Inference Time': None}
+
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        if model_name == 'Ensemble Genetic Programming':
+            model = EnsembleGeneticProgramming()
+            hyper_parameters = egp_hyper_parameters
+        else:
+            model = ExtraTreeRegressor()
+            hyper_parameters = dict(max_depth=range(1, 9), min_samples_split=range(2, 8), splitter=['random', 'best'])
+        random_search_cv = RandomizedSearchCV(model, hyper_parameters, n_iter=50, cv=3).fit(X_train, y_train)
+
+        test_cross_data['Hyper-Parameters Values'] = str(random_search_cv.best_params_)\
+            .replace('\'', '').replace('{', '').replace('}', '')
+
+        fold_model = random_search_cv.best_estimator_
+
+        time_before_train = datetime.now()
+        fold_model.fit(X_train, y_train)
+        train_time = datetime.now() - time_before_train
+        test_cross_data['Training Time'] = f"{train_time.microseconds} microseconds"
+
+        y_pred = fold_model.predict(X_test)
+        test_cross_data['Mean Squared Error'] = mean_squared_error(y_test, y_pred)
+        test_cross_data['Mean Absolute Error'] = mean_absolute_error(y_test, y_pred)
+        test_cross_data['Median Absolute Error'] = median_absolute_error(y_test, y_pred)
+        test_cross_data['R2 Score'] = r2_score(y_test, y_pred)
+        test_cross_data['Explained Variance Score'] = explained_variance_score(y_test, y_pred)
+
+        time_before_predict = datetime.now()
+        if len(X) < 1000:
+            fold_model.predict(X)
+        else:
+            fold_model.predict(X.iloc[:1000])
+        predict_time = datetime.now() - time_before_predict
+        test_cross_data['Inference Time'] = f"{predict_time.microseconds} microseconds"
+
+        # Putting all together
+        test_data.append(test_cross_data)
+        outer_fold_index += 1
+    return test_data
 
 
 def main():
-    df = pd.read_csv(f'{working_dir}/regressionDatasets/Admission_Predict_kaggle.csv')
-    X = df.iloc[:, :-1]
-    y = df.iloc[:, -1]
-    reg = RandomForestRegressor()
-    reg.fit(X, y)
-    reg.predict(X)
-    egp = EnsembleGeneticProgramming()
-    egp.fit(X, y)
-    egp.predict(X)
-
-
-def eval_dataset():
-    pass
+    test_df = []
+    dataset_index = 1
+    egp_hyper_parameters = dict(num_trees=range(50, 100, 50), num_forest=range(2, 20, 2),
+                                max_generations=range(2, 10, 1))
+    egp_str = str(egp_hyper_parameters).replace('\'', '').replace('{', '').replace('}', '')
+    print("EGP hyper-parameters are: " + egp_str)
+    for dataset_path in os.listdir(f'{working_dir}/regressionDatasets/'):
+        print(f"dataset index: {dataset_index}, dataset name: {dataset_path}")
+        test_df.extend(eval_dataset(dataset_path, 'Ensemble Genetic Programming', egp_hyper_parameters, egp_str))
+        test_df.extend(eval_dataset(dataset_path, 'Extra Tree Regressor', egp_hyper_parameters, egp_str))
+        dataset_index += 1
+    pd.DataFrame(test_df).to_csv(f'results_{egp_str}.csv', index=False)
 
 
 if __name__ == '__main__':
+    print('****** START MAIN ******')
     main()
+    print('****** END MAIN ******')
