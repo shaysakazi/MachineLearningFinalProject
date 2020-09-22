048becea716d42d34796ecc7d0da104913434a02
Shay Sakazi <sakazis@post.bgu.ac.il>
2020-09-22 15:45:16 +0300

    finish project

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# MachineLearningFinalProject
+# Machine Learning Final Project
 
-This is the final project readme file
+This file contains the instruction of the code pipeline.
+ensemble_genetic_programming.py is our implementation of the eGP algorithm, as shown in the article:
+https://arxiv.org/abs/2001.07553.
+We implement as a scikit-learn regressor model API to fit the model simply use the fit function.
+To predict an instance use the predict function.
+All other functions are the inner functions of the algorithm.
 
+meta_model.py is our implementation of XGBoost on the meta-features data set. We added a label column, which indicates which of the algorithm has won. One if our eGP implementation get better MSE value than Extra tree regressor
+
+main.py is the file that runs the required tests.
+All functions are documented in a pydoc format.
+
+Authors:
+Shay Sakazi
+Ido Sakazi
--- src/ensemble_genetic_programming.py
+++ src/ensemble_genetic_programming.py
@@ -10,7 +10,20 @@ import copy
 
 
 class EnsembleGeneticProgramming:
+    """
+    Ensemble Genetic Programming class implemented with a scikit-learn regressor API
+    """
     def __init__(self, num_trees=100, num_forest=100, max_generations=10, k=2):
+        """
+        Constructor of the eGP class
+        :param num_trees: number of trees which forests are composed of.
+        :param num_forest: number of forest to construct
+        :param max_generations: number of generation to run
+        :param k: number of forest to compare in the
+
+        max_time: stop algorithm session if it surpass its max time limit
+        voting_forests: final voting forests
+        """
         self.num_trees = num_trees
         self.num_forest = num_forest
         self.max_generations = max_generations
@@ -25,6 +38,12 @@ class EnsembleGeneticProgramming:
         # pd.DataFrame(self.illustrate_forest).to_csv('ill.csv',index=False)
 
     def _egp(self, X, y):
+        """
+        This function is the main controller of the algorihm, this is where the stages are taking place
+        :param X: dataset features
+        :param y: dataset target value
+        :return: forest list which will be used for predicting
+        """
         forest_list = self._generate_forests(X)  # list of (str, estimator) tuples
         # self.gn = 'Before generation'
         # self.s = 'Generate forests'
@@ -33,33 +52,37 @@ class EnsembleGeneticProgramming:
         # for forest in forest_list:
         #     self._add_illustrate(forest, X, y, self.gn, self.s)
         g = 0
+        start_time = datetime.now()
+        time_delta = datetime.now() - start_time
         # self.gn = g
-        while g < self.max_generations:
+        while g < self.max_generations and time_delta.seconds < 5:
             # print(f"Generation index: {g}, number of forests: {len(forest_list)},"
             #       f" max_generations: {self.max_generations}")
-            time = datetime.now()
+
             forest_parents = self._select_forests(forest_list, X, y)  # choose N forests
             # self.s = f"Select forests => best selected forests"
             # for forest in forest_parents:
             #     self._add_illustrate(forest, X, y, self.gn, self.s)
-            time = datetime.now() - time
+            # time = datetime.now() - time
             # print(f"Finish selection on Generation index: {g} after {time.seconds} seconds")
-            time = datetime.now()
+            # time = datetime.now()
             forest_offsprings = self._breeding(forest_parents)  # create N off springs
-            time = datetime.now() - time
+            # time = datetime.now() - time
             # print(f"Finish breeding on Generation index: {g} after {time.seconds} seconds")
-            time = datetime.now()
+            # time = datetime.now()
             # self.s = 'all forest before pruning'
             # for forest in forest_offsprings:
             #     self._add_illustrate(forest, X, y, self.gn, self.s)
             forest_list = self._prune(forest_offsprings, X, y)  # prune 2N (N from forest selection and N from off
             # springs) into N forests
-            time = datetime.now() - time
+            # time = datetime.now() - time
             # print(f"Finish pruning on Generation index: {g} after {time.seconds} seconds")
             # self.s = 'best forests after pruning'
             # for forest in forest_list:
             #     self._add_illustrate(forest, X, y, self.gn, self.s)
             g += 1
+            time_delta = datetime.now() - start_time
+            print(time_delta.seconds)
             # self.gn = g
 
         # pd.DataFrame(self.illustrate_forest).to_csv('illustrate.csv', index=False)
@@ -76,6 +99,11 @@ class EnsembleGeneticProgramming:
     #     return best_forest[0], best_forest[1].fit(X[dataset_features], y)
 
     def _generate_forests(self, X):
+        """
+        This function iterate over number of desired forest to generated
+        :param X: dataset features
+        :return: generated forests list
+        """
         forest_list = []
         for forest_index in range(self.num_forest):
             new_forest = self._generate_forest(X)
@@ -87,6 +115,12 @@ class EnsembleGeneticProgramming:
         return forest_list
 
     def _generate_forest(self, X):
+        """
+        This function generate forest randomly, where takes in a random propability the forest features
+        and choose randomly the dataset features.
+        :param X: dataset features
+        :return: generated forest => tuple consist forest features and dataset features
+        """
         dataset_features = X.columns
         forest_features = {'max_depth': range(2, 10),
                            'min_samples_split': range(2, 10),
@@ -117,6 +151,11 @@ class EnsembleGeneticProgramming:
         return rf_features, rf_reg
 
     def _choose_features(self, features):
+        """
+        This fucntion decide which dataset features a generated forest will have
+        :param features: dataset features
+        :return: the chosen dataset features
+        """
         num_of_features = random.randrange(len(features)) + 1
         used_features = []
         for _ in range(num_of_features):
@@ -128,6 +167,14 @@ class EnsembleGeneticProgramming:
         return used_features
 
     def _select_forests(self, forest_list, X, y):
+        """
+        This function yields the best forests that wins the tournament selection
+        The amount of forest depends on the num_forest parameter
+        :param forest_list:
+        :param X: dataset features
+        :param y: dataset target value 
+        :return: forest tournament selection winners
+        """
         forest_parents = []
         for _ in range(self.num_forest):
             # time = datetime.now()
@@ -140,6 +187,14 @@ class EnsembleGeneticProgramming:
         return forest_parents
 
     def _tournament_selection(self, forest_list, X, y):
+        """
+        This function create a tournament between k randomly chosen forest from the generated forest
+        it yields the best forest between the randomly selected
+        :param forest_list: this is the list which randomly choose forest from
+        :param X: dataset features
+        :param y: dataset target value 
+        :return: forest tournament selection winner
+        """
         best = None
         best_mse = 0
         for i in range(self.k):
@@ -155,6 +210,11 @@ class EnsembleGeneticProgramming:
         return best
 
     def _breeding(self, forest_parents):
+        """
+        This function create offspring form a given list of parents
+        :param forest_parents: list of parents, from this list the offspring will created
+        :return: created offsprings
+        """
         offsprings = copy.deepcopy(forest_parents)
         for i in range(self.num_forest):
             # self.s = f"breeding index {i}"
@@ -163,6 +223,11 @@ class EnsembleGeneticProgramming:
         return offsprings
 
     def _breed(self, forest_parents):
+        """
+        This function created a new offspring
+        :param forest_parents: a list of parents candidates
+        :return: created offspring
+        """
         parent_1 = random.choice(forest_parents)
         parent_2 = random.choice(forest_parents)
 
@@ -179,6 +244,12 @@ class EnsembleGeneticProgramming:
         return offspring
 
     def _uniform_crossover(self, parent_1, parent_2):
+        """
+        This function generated a new offspring from a given two parents using uniform cross over
+        :param parent_1: first parent
+        :param parent_2: second parent
+        :return: new offspring
+        """
         offspring_forest_features = {'max_depth': None, 'min_samples_split': None, 'min_weight_fraction_leaf': None,
                                      'max_features': None}
         parent_1_forest_features = {'max_depth': parent_1[1].max_depth,
@@ -217,6 +288,12 @@ class EnsembleGeneticProgramming:
         return rf_features, rf_reg
 
     def _mutate(self, offspring):
+        """
+        This function applied a mutation for an offspring means the offspring can have a feature that is parents
+        did not have.
+        :param offspring: the new generated offspring
+        :return: offspring after mutation has applied
+        """
         mutate_feature = random.choice(['max_depth', 'min_samples_split', 'min_weight_fraction_leaf', 'max_features'])
         forest_features = {'max_depth': range(2, 10),
                            'min_samples_split': range(2, 10),
@@ -240,6 +317,13 @@ class EnsembleGeneticProgramming:
         return offspring_features, offspring[1]
 
     def _prune(self, forest_offsprings, X, y):
+        """
+        This function takes the best forests from the forest_offsprings list
+        :param forest_offsprings: list of forest candidates
+        :param X: dataset feature
+        :param y: dataset target value 
+        :return: The best forests from the list of forest candidates
+        """
         forest_offsprings_score = {i: self._eval_forest(forest_offsprings[i], X, y)
                                    for i in range(len(forest_offsprings))}
 
@@ -253,6 +337,13 @@ class EnsembleGeneticProgramming:
         return [forest_offsprings[forest_index[0]] for forest_index in prune_forest]
 
     def _eval_forest(self, ind, X, y):
+        """
+        evaluate forest
+        :param ind: forest to evaluate
+        :param X: dataset features
+        :param y: dataset target value 
+        :return: the forest mean negative mean squared error in a 3 cross validation
+        """
         dataset_features = ind[0]['dataset_features']
         forest = ind[1]
         mse_fold = cross_val_score(forest, X[dataset_features], y, scoring='neg_mean_squared_error', cv=3)
@@ -260,6 +351,13 @@ class EnsembleGeneticProgramming:
         return np.mean(mse_fold) + np.std(mse_fold)
 
     def fit(self, X, y):
+        """
+        This function is similar to scikit-learn API which use X as data features and y for labels
+        :param X: dataset features
+        :param y: dataset target value 
+
+        fit the voting forest list
+        """
         # print("===START===")
         self.voting_forests = self._egp(X, y)
         for forest in self.voting_forests:
@@ -268,6 +366,11 @@ class EnsembleGeneticProgramming:
         # print("===END===")
 
     def predict(self, X):
+        """
+        This function is similar to scikit-learn API which predict the target value from a given dataset features
+        :param X: dataset features
+        :return: target value
+        """
         if self.voting_forests is None:
             raise NotFittedError("This EnsembleGeneticProgramming instance is not fitted yet. "
                                  "Call 'fit' with appropriate arguments before using this estimator.")
@@ -281,6 +384,12 @@ class EnsembleGeneticProgramming:
         return np.array([np.mean([x for x in c]) for c in columns])
 
     def score(self, X, y):
+        """
+        This function is similar to scikit-learn API which yields the model score
+        :param X: dataset features       
+        :param y: dataset target value   
+        :return: model score
+        """
         if self.voting_forests is None:
             raise NotFittedError("This EnsembleGeneticProgramming instance is not fitted yet. "
                                  "Call 'fit' with appropriate arguments before using this estimator.")
@@ -291,10 +400,20 @@ class EnsembleGeneticProgramming:
         return np.mean(predict_scores)
 
     def get_params(self, deep=True):
+        """
+        This function is similar to scikit-learn API return the model params
+        :param deep: scikit-learn API parameter
+        :return: return the model params
+        """
         return {"num_trees": self.num_trees, "num_forest": self.num_forest, "max_generations": self.max_generations,
                 "k": self.k}
 
     def set_params(self, **parameters):
+        """
+        This function is similar to scikit-learn API sets the model params
+        :param parameters: params to set
+        :return: model after setting the params
+        """
         for parameter, value in parameters.items():
             setattr(self, parameter, value)
         return self
